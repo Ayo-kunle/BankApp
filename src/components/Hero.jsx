30e14f7958c90f97c13c6c67c4946cec201d635b
ayo-kunle <edwardsayokunle@gmail.com>
2023-01-25 21:33:48 +0100

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => (
         Payment Method.
       </h1>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Our team of experts uses a methodology to identify the credit cards most
+        Our team of experts use a methodology to identify the credit cards most
         likely to fit your needs. We examine annual percentage rates, annual
         fees.
       </p>
